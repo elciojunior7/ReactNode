3e73b0040b20cd5e72e5662e074ee73c657e9ea3
unknown <elciosouza.junior@gmail.com>
2020-03-25 19:14:39 -0300

    Nodemon

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,10 @@ const express = require("express"); //importar o módulo express
 
 const app = express(); //instanciar aplicação
 
-app.get("/", (request, response) => {
-    //return response.send("Hello World"); //imprime Hello World na tela
+app.use(express.json());
+
+app.get("/", (request, response) => {  
+    //return response.send("Hello wwWorld"); //imprime Hello World na tela
     return response.json({                //Json devolvido conforme API REST
         evento: "Semana OmniStack",
         aluno: "Elcio Jr"
